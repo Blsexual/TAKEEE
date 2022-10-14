2c3be264ef0f372e0ce719548b3ed4f52d7f2cc5
Nixeff <theodore.folkesson@gmail.com>
2022-10-14 14:43:38 +0200

    Commented properly

--- Wiki/wiki_page_create.php
+++ Wiki/wiki_page_create.php
@@ -2,17 +2,23 @@
     session_start();
     require_once("../db.php");
 
-    $user = $_REQUEST["user"]; // user = user ID
-    $wiki = $_GET["wiki"]; // wiki = wiki ID
+/*-----------------------------------------------------------
+        Variabels
+-----------------------------------------------------------*/
+    $user = $_REQUEST["user"];      // user = user ID
+    $wiki = $_GET["wiki"];          // wiki = wiki ID
     $title = $_GET["title"];
-    $contents = $_GET["contents"]; // html?
+    $contents = $_GET["contents"];  // html?
 
-    $date = getdate(); // get the date in a array 
-    $todayDate = $date["year"]."-".$date["mon"]."-".$date["mday"]; // Creates a date variable the database can handle (yyyy-mm-dd)
+    $date = getdate();              // get the date in a array 
+    $todayDate = $date["year"]."-".$date["mon"]."-".$date["mday"];      // Creates a date variable the database can handle (yyyy-mm-dd)
 
+/*-----------------------------------------------------------
+        Connection
+-----------------------------------------------------------*/
     $sql = "INSERT INTO wiki_entry (wID,uID,title,contents,date) VALUES($wiki,$user,'$title','$contents','$todayDate')";
 
-    $result = $conn->query($sql);
+    $result = $conn->query($sql);       // Sends question to database
     echo $result;
     
 ?>
--- Wiki/wiki_page_remove.php
+++ Wiki/wiki_page_remove.php
@@ -2,13 +2,8 @@
     session_start();
     require_once("../db.php");
 
-    $user = $_REQUEST["user"]; // user = user ID
-    $wiki = $_GET["wiki"]; // wiki = wiki ID
-    $title = $_GET["title"];
-    $contents = $_GET["contents"]; // html?
-
-    $date = getdate(); // get the date in a array 
-    $todayDate = $date["year"]."-".$date["mon"]."-".$date["mday"]; // Creates a date variable the database can handle (yyyy-mm-dd)
+    $user = $_REQUEST["user"];  // user = user ID
+    $page = $_GET["page"];      // wiki_entry ID
 
     $sql = "INSERT INTO wiki_entry (wID,uID,title,contents,date) VALUES($wiki,$user,'$title','$contents','$todayDate')";
 
