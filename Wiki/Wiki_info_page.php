81f3851a6852818a1608b78313339e2fe878f837
Nixeff <theodore.folkesson@gmail.com>
2022-10-19 13:00:34 +0200

    DONE

--- Wiki/Wiki_info_page.php
+++ Wiki/Wiki_info_page.php
@@ -6,7 +6,7 @@
     ----------------------------------*/
     $ID = $_REQUEST["ID"];
     if (empty($_GET)){
-        $data = ["error"=>"wrong username or password was given"];
+        $data = ["error"=>"No ID was given"];
         $type = "error";
         $return = ["version"=>$version,"type"=>$type,"data"=>$data];
         die(json_encode($return));
@@ -18,10 +18,38 @@
     /*----------------------------------
         Fetch data
     ----------------------------------*/
-    while ($row = mysqli_fetch_assoc($result)){
-        $emparray[] = $row;
+    if ($result->num_rows > 0) {
+        while ($row = mysqli_fetch_assoc($result)){
+            $emparray[] = $row;
+        }
+    } else{
+        $data = ["error"=>"We could not find the page you are looking for"];
+        $type = "error";
+        $return = ["version"=>$version,"type"=>$type,"data"=>$data];
+        die(json_encode($return));
+    }
+
+    $sql = "SELECT MAX(editDate) AS editDate,title,contents,date,wID,oID,uID,ID FROM wiki_entry_history where (oID) = $ID";
+    $result = mysqli_query($conn, $sql);
+
+    $test = [];
+    if ($result->num_rows > 0) {
+        while ($row = mysqli_fetch_assoc($result)){
+            $test[] = $row;
+        }
+    } else{
+        $type = "ok";
+        $return = ["version"=>$version,"type"=>$type,"data"=>$emparray];
+        die(json_encode($return)); // Send data as json
+    }
+    if($test[0]["ID"] == NULL){
+        $type = "ok";
+        $return = ["version"=>$version,"type"=>$type,"data"=>$emparray];
+        die(json_encode($return)); // Send data as json
     }
     $type = "ok";
-    $return = ["version"=>$version,"type"=>$type,"data"=>$emparray];
-    die(json_encode($return)); // Send data as json
+        $return = ["version"=>$version,"type"=>$type,"data"=>$test];
+        die(json_encode($return)); // Send data as json
+    
+    
 ?>
