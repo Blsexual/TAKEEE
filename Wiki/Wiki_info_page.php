987d5eab62c80f30cf48e936d1cef44e3aa33e14
Nixeff <theodore.folkesson@gmail.com>
2022-10-19 11:55:09 +0200

    Komentarer

--- Wiki/Wiki_info_page.php
+++ Wiki/Wiki_info_page.php
@@ -1,11 +1,15 @@
 <?php
     require_once("../db.php");
+    $version = "0.0.1"; 
     /*----------------------------------
         Variables
     ----------------------------------*/
     $ID = $_REQUEST["ID"];
     if (empty($_GET)){
-        die("Error: Inget ID data har identifierats");
+        $data = ["error"=>"wrong username or password was given"];
+        $type = "error";
+        $return = ["version"=>$version,"type"=>$type,"data"=>$data];
+        die(json_encode($return));
     }
     $sql = "SELECT * FROM wiki_entry where (ID) = $ID";
     $result = mysqli_query($conn, $sql);
@@ -17,6 +21,7 @@
     while ($row = mysqli_fetch_assoc($result)){
         $emparray[] = $row;
     }
-
-    echo json_encode($emparray); // Send data as json
+    $type = "ok";
+    $return = ["version"=>$version,"type"=>$type,"data"=>$emparray];
+    die(json_encode($return)); // Send data as json
 ?>
--- Wiki/wiki_page_remove.php
+++ Wiki/wiki_page_remove.php
@@ -23,16 +23,37 @@
             $res = $row;
         }
     } else{
-        die(json_encode("We can not find the page in question"));
+        // JSON Return
+        $data = ["error"=>"we can not find the page you are looking for"];
+        $type = "error";
+        $return = ["version"=>$version,"type"=>$type,"data"=>$data];
+        die(json_encode($return));
     }
 
-    if($res["admin"][0] == "1" ){
+    if($res["admin"][0] == "1" ){   // If you are a admin
         $sql = "DELETE FROM wiki_entry WHERE wiki_entry.ID = $page";
         $conn->query($sql);
-    } else if ($res["endUser"][0] == "1"){
+
+        // JSON Return
+        $data = ["ok"=>"succsess"];
+        $type = "ok";
+        $return = ["version"=>$version,"type"=>$type,"data"=>$data];
+        die(json_encode($return));
+    } else if ($res["endUser"][0] == "1"){  // If you are a enduser
         $sql = "DELETE FROM wiki_entry WHERE wiki_entry.ID = $page";
         $conn->query($sql);
+
+        // JSON Return
+        $data = ["ok"=>"succsess"];
+        $type = "ok";
+        $return = ["version"=>$version,"type"=>$type,"data"=>$data];
+        die(json_encode($return));
     }else{          // Basicly if something went very wrong
-        die(json_encode("Du har inte rätt att tabort den här sidan"));
+
+        // JSON Return
+        $data = ["error"=>"you are not allowed to remove this entry"];
+        $type = "error";
+        $return = ["version"=>$version,"type"=>$type,"data"=>$data];
+        die(json_encode($return));
     } 
 ?>
