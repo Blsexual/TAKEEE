1caf580c09dcc3f1afcd9a0b33b77935f121f3b5
Erik <erik.gustafsson03@elev.ntig.se>
2022-10-19 12:00:12 +0200

    lunch break

--- Blog/blog_entry.php
+++ Blog/blog_entry.php
@@ -1,24 +1,57 @@
 
 <?php
     require_once("../db.php");
+    $version = "0.1.1";
 
     /*---------------------------------------
               creating new entries
     ----------------------------------------*/
 
-    $title = $_GET["title"];
-    $content = $_GET["content"];  // gets the varibales
+    $title = "title";
+    $content = "content";  // sets base variables
+    $bid = 0;
+    $uid = 0;
+
+    
+    if(!empty($_GET['title'])){
+        $title = $_GET['title'];
+    }
+    
+    if(!empty($_GET['content'])){
+        $content = $_GET['content'];
+    }
+    
+    if(!empty($_GET['bid'])){
+        $bid = $_GET['bid'];
+    }
+ 
+    if(!empty($_GET['uid'])){
+        $uid = $_GET['uid'];
+    }
+
+
+
+    
+
+
+    $content = $_GET["content"];  // gets the variables
     $bid = $_GET["bid"];
     $uid = $_GET["uid"];
     $date = date("Y/m/d");
 
-    if ($bid != NULL){
+    if ($bid != 0){
       $makeblog = "INSERT INTO blog_entry(title,contents,date,bID,uID) VALUES ('$title','$content','$date','$bid','$uid')";       //creates the new entries
       $result = $conn->query($makeblog);   
     }
-    else{
+    else if($bid = 0){
       $makeentry = "INSERT INTO blog(title,description,date,uID) VALUES ('$title','$content','$date','$uid')";       //creates the new entries
       $result = $conn->query($makeentry); 
+    }
+    else{
+      $type = "Error";
+      $dataB = array("wrong inputs");
+      $return = ["Version"=>$version,"Type"=>$type,"Blogg"=>$dataB];
+      die(json_encode($return));
     }
      
     //   ?title=x&content=x&uid=x
