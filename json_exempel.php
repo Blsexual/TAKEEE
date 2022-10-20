c8ebdcb7bd83101a0fc7be553afe3c65d618f00b
Bisexual | (Kev) <101113513+Blsexual@users.noreply.github.com>
2022-10-20 08:27:37 +0200

    Update json_exempel.php

--- json_exempel.php
+++ json_exempel.php
@@ -1,60 +1,31 @@
-<form method='post'>
-    Namn <input type='input' name='Namn' ><br>
-    Efternamn <input type='input' name='Efternamn' ><br>
-    E-Post <input type='email' name='E-Post' ><br>
-    <input type='submit' value='Skicka'>
-</form>
+<?php #Message on error
+    function errorWrite($version,$error){
+        $return = ["Version"=>$version,"Type"=>"Error","Data"=>"$error"];
+        die(json_encode($return));
+    }
+?>
+
+<?php #How to display the data in blog
+    function blogWrite($version,$blog,$blogEntry){
+        $data = ["Blog"=>$blog,"Blog entry"=>$blogEntry];
+        $contents = ["Version"=>$version,"Type"=>"Ok","Data"=>$data];
+        die(json_encode($contents));
+    }
+?>
 
 <?php
-    print_r($_POST);
-    echo "<br>";
-    echo json_encode($_POST);
-    echo "<br>";
-    echo "<br>";
-    $Data = '{"Namn":"'.$_POST['Namn'].'","Efternamn":"'.$_POST['Efternamn'].'","E-Post":"'.$_POST['E-Post'].'"}';
-    echo $Data;
-    echo "<br>";
-    echo print_r(json_decode($Data));
-
-    echo "<br>";
-    echo "<br>";
-
-
-    $DataFull = array();
-    if (isset($_POST['Namn'])){
-        array_push($DataFull,$_POST['Namn']);
+    function calendarWrite($version,$event){
+        $data = ["My events"=>$event];
+        $contents = ["Version"=>$version,"Type"=>"Ok","Data"=>$data];
+        die(json_encode($contents));
     }
-    if (isset($_POST['Efternamn'])){
-        array_push($DataFull,$_POST['Efternamn']);
+?>
+
+<?php #How to display the data in blog
+    function wikiWrite($version,$wiki, $wikiEntry){
+        $data = ["Wiki"=>$wiki,"Wiki entry"=>$wikiEntry];
+        $contents = ["Version"=>$version,"Type"=>"Ok","Data"=>$data];
+        die(json_encode($contents));
     }
-    if (isset($_POST['E-Post'])){
-        array_push($DataFull,$_POST['E-Post']);
-    }
-    print_r($DataFull);
-    $namn = $_POST['Namn'];
 
-    echo "<br>";
-    echo "<br>";
-
-    $families = array(
-        "Griffin"=>array(
-            "Peter",
-            "Lois",
-            "Megan"
-        ),
-        "Quagmire"=>array(
-            "Glenn"
-        ),
-        "Brown"=>array(
-            "Cleveland",
-            "Loretta",
-            "Junior"
-        )
-    );   
-    
-    print_r($families);
-
-    echo "<br>";
-    echo "<br>";
-
-    echo json_encode($families);
+?>
