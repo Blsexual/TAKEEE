a6d6b16a180a20202917425122c308e47a2e9fb5
BreadWithSoup <erik.post@elev.ga.ntig.se>
2022-10-20 15:30:09 +0200

    Can now invite

--- Calender/calender_delete_event.php
+++ Calender/calender_delete_event.php
@@ -1,11 +1,11 @@
 <?php
-    if(!empty($_GET['eventID'])){
+    if(!empty($_GET['action'])){
         $ID = $_GET['eventID'];
         $stmt = $conn->prepare("SELECT `ID` FROM `event` WHERE `ID`=? AND `uID`=?");
         $stmt->bind_param("ii", $ID,$uID);
-    
         $stmt->execute();
         $result = $stmt->get_result();
+
         if ($result->num_rows > 0) {
             $stmt->close();
             $stmt = $conn->prepare("DELETE FROM `event` WHERE `ID`=? AND `uID`=?");
@@ -19,5 +19,6 @@
             errorWrite($version,"Selected event doesn't exist");
         }
     }
+    
     errorWrite($version,"Didn't specify which event to delete");
 ?>
--- Calender/calender_event_decline.php
+++ Calender/calender_event_decline.php
@@ -1,5 +1,5 @@
 <?php
-    if(!empty($_GET['eventDelete'])){ 
+    if(!empty($_GET['action'])){ 
 
     }
 ?>
