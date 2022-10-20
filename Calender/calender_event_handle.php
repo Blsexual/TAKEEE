6f01d185a512c562c654eeb1ff555ad9b706f1c8
BreadWithSoup <erik.post@elev.ga.ntig.se>
2022-10-20 15:53:18 +0200

    eventHandle

--- Calender/calender_event_handle.php
+++ Calender/calender_event_handle.php
@@ -1,9 +1,10 @@
 <?php
-    if(!empty($_GET['action'])){ 
-        if(!empty($_GET['eventAccept'])){
+    if(!empty($_GET['eventHandle'])){ 
+        $eventHandle = $_GET['eventHandle'];
+        if($eventHandle == "eventAccept"){
             require("calender_event_accept.php");
         }
-        if(!empty($_GET['eventDecline'])){
+        if($eventHandle == "eventDecline"){
             require("calender_event_decline.php");
         }
     }
