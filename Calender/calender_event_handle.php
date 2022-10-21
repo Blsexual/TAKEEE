f835d5e2536e777a28549eacc72f633ea717ffc8
BreadWithSoup <erik.post@elev.ga.ntig.se>
2022-10-21 09:46:04 +0200

    handle

--- Calender/calender_event_handle.php
+++ Calender/calender_event_handle.php
@@ -1,12 +1,13 @@
 <?php
-    if(!empty($_GET['eventHandle'])){ 
-        $eventHandle = $_GET['eventHandle'];
-        if($eventHandle == "eventAccept"){
+    if(!empty($_GET['handle'])){ 
+        $eventHandle = $_GET['handle'];
+        if($eventHandle == "accept"){
             require("calender_event_accept.php");
         }
-        if($eventHandle == "eventDecline"){
+        if($eventHandle == "decline"){
             require("calender_event_decline.php");
         }
         errorWrite($version,"Not a valid action, either accept or decline");
     }
+    errorWrite($version,"Specify handle");
 ?>
