fe88a25b1e4ffa7dd3e68a5a60f6886eab9a5c26
Erik <erik.gustafsson03@elev.ntig.se>
2022-10-21 15:27:51 +0200

    slut av dagen

--- Blog/blog_create.php
+++ Blog/blog_create.php
@@ -13,6 +13,7 @@
         $content = "No content";  // sets base variables
         $bID = 0;
         $uID = 0;
+        $token = "";
 
         if(!empty($_GET['title'])){
             $title = $_GET['title'];
@@ -26,34 +27,50 @@
             $bID = $_GET['bID'];
         }
 
-        $token = $_GET['token'];
-        $uID = $_GET['uID'];
+        if(!empty($_GET['token'])){
+            $token = $_GET['token'];
+        }
+
+        if(!empty($_GET['uID'])){
+            $uID = $_GET['uID'];
+        }
+        
         
 
         $date = date("Y/m/d H:i:s");
 
-        checkToken($token,$uID,"010",$version, $conn);
+        $res = checkToken($token,$uID,"010",$version, $conn);
+        print_r($res);
     #
 
     /*---------------------------------------
         creating new entries and blogs
     ----------------------------------------*/
 
-        if ($bID != 0){
-            $makeentry = "INSERT INTO blog_entry(title,contents,date,bID,uID) VALUES ('$title','$content','$date','$bID','$uID')";       //creates the new entries
-            $result = $conn->query($makeentry); 
-            $data = ["Action"=>"Entry created"];
-            jsonWrite($version,$data);
+        if ($res["UserType"] == "endUser"){
+            if($bID == 0){
+                $makeblog = "INSERT INTO blog(title,description,date,uID) VALUES ('$title','$content','$date','$uID')";       //creates the new blogs
+                $result = $conn->query($makeblog); 
+                $data = ["Action"=>"Blog created"];
+                jsonWrite($version,$data);
+            }
+            else{
+                errorWrite($version,"Wrong inputs");
+            }
         }
-        else if($bID == 0){
-            $makeblog = "INSERT INTO blog(title,description,date,uID) VALUES ('$title','$content','$date','$uID')";       //creates the new blogs
-            $result = $conn->query($makeblog); 
-            $data = ["Action"=>"Blog created"];
-            jsonWrite($version,$data);
-        }
-        else{
-            errorWrite($version,"Wrong inputs");
-        }
+        else if ($res["UserType"] == "admin"){
+            if ($bID != 0){
+                $makeentry = "INSERT INTO blog_entry(title,contents,date,bID,uID) VALUES ('$title','$content','$date','$bID','$uID')";       //creates the new entries
+                $result = $conn->query($makeentry); 
+                $data = ["Action"=>"Entry created"];
+                jsonWrite($version,$data);
+            }
+            else{
+                errorWrite($version,"Wrong inputs");
+            }
+        } 
+
+
     #
     //   ?title=x&content=x&uID=x
 ?>
