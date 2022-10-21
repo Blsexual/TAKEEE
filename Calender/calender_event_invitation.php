34ab2edc5e02f37331134f2417f2da2dd80cf140
BreadWithSoup <erik.post@elev.ga.ntig.se>
2022-10-21 11:05:59 +0200

    comments in event invitation files

--- Calender/calender_event_invitation.php
+++ Calender/calender_event_invitation.php
@@ -1,56 +1,89 @@
 <?php
-    if(!empty($_GET['eventID'])){
-        $eID = $_GET['eventID'];
-    }
-    else{
-        errorWrite($version,"Didn't specify which event to invite to");
-    }
+    /*----------------------------------------------------------------------
+        Checks if eventID is set
+    ----------------------------------------------------------------------*/
+        if(!empty($_GET['eventID'])){
+            $eID = $_GET['eventID'];
+        }
+        else{
+            errorWrite($version,"Didn't specify which event to invite to");
+        }
+    #
     
-    $stmt = $conn->prepare("SELECT `ID` FROM `event` WHERE `ID`=? AND `uID`=?");
-    $stmt->bind_param("ii", $eID,$uID);
-    $stmt->execute();
-    $result = $stmt->get_result();
+    /*----------------------------------------------------------------------
+        Checks if the event exists
+    ----------------------------------------------------------------------*/
+        $stmt = $conn->prepare("SELECT `ID` FROM `event` WHERE `ID`=? AND `uID`=?");
+        $stmt->bind_param("ii", $eID,$uID);
+        $stmt->execute();
+        $result = $stmt->get_result();
 
-    if ($result->num_rows < 1) {           
-        errorWrite($version,"Could not find event");
-    }
-    $stmt->close();
+        if ($result->num_rows < 1) {           
+            errorWrite($version,"Could not find event");
+        }
+        $stmt->close();
+    #
 
-    if(!empty($_GET['recipientID'])){
-        $rID = $_GET['recipientID'];
-    }
-    else{
-        errorWrite($version,"Didn't specify which recipient to invite");
-    }
+    /*----------------------------------------------------------------------
+        Checks if recipentID is set
+    ----------------------------------------------------------------------*/
+        if(!empty($_GET['recipientID'])){
+            $rID = $_GET['recipientID'];
+        }
+        else{
+            errorWrite($version,"Didn't specify which recipient to invite");
+        }
+    #
 
-    $stmt = $conn->prepare("SELECT `ID`, `name` FROM `user` WHERE `ID`=?");
-    $stmt->bind_param("i", $rID);
-    $stmt->execute();
-    $result = $stmt->get_result();
+    /*----------------------------------------------------------------------
+        Checks if the recipentID set exists
+    ----------------------------------------------------------------------*/
+        $stmt = $conn->prepare("SELECT `ID`, `name` FROM `user` WHERE `ID`=?");
+        $stmt->bind_param("i", $rID);
+        $stmt->execute();
+        $result = $stmt->get_result();
 
-    if ($result->num_rows < 1) {           
-        errorWrite($version,"Could not find recipient");
-    }
-    elseif($result->num_rows > 0){
-        $row = $result->fetch_assoc();
-        $name = $row['name'];
-    }
-    $stmt->close();
+        if ($result->num_rows < 1) {           
+            errorWrite($version,"Could not find recipient");
+        }
+    #
 
-    $stmt = $conn->prepare("SELECT `ID` FROM `event_invitation` WHERE `rID`=? AND `eID`=?");
-    $stmt->bind_param("ii", $rID,$eID);
-    $stmt->execute();
-    $result = $stmt->get_result();
+    /*----------------------------------------------------------------------
+        Saves name of the recipent if recipentID exists
+    ----------------------------------------------------------------------*/
+        elseif($result->num_rows > 0){
+            $row = $result->fetch_assoc();
+            $name = $row['name'];
+        }
+        $stmt->close();
+    #
+    
+    /*----------------------------------------------------------------------
+        Checks if the recipient is already invited
+    ----------------------------------------------------------------------*/
+        $stmt = $conn->prepare("SELECT `ID` FROM `event_invitation` WHERE `rID`=? AND `eID`=?");
+        $stmt->bind_param("ii", $rID,$eID);
+        $stmt->execute();
+        $result = $stmt->get_result();
 
-    if ($result->num_rows > 0) {           
-        errorWrite($version,"Have already invited recipient");
-    }
-    $stmt->close();
+        if ($result->num_rows > 0) {           
+            errorWrite($version,"Have already invited recipient");
+        }
+        $stmt->close();
+    #
 
-    $stmt = $conn->prepare("INSERT INTO `event_invitation` (rID, eID) VALUES (?,?)");
-    $stmt->bind_param("ii", $rID, $eID);
-    $stmt->execute();
+    /*----------------------------------------------------------------------
+        Creates invitation
+    ----------------------------------------------------------------------*/
+        $stmt = $conn->prepare("INSERT INTO `event_invitation` (rID, eID) VALUES (?,?)");
+        $stmt->bind_param("ii", $rID, $eID);
+        $stmt->execute();
+    #
 
-    $data = ["Action"=>"Invited $name to event"];
-    jsonWrite($version,$data);
+    /*----------------------------------------------------------------------
+        Outputs json
+    ----------------------------------------------------------------------*/
+        $data = ["Action"=>"Invited $name to event"];
+        jsonWrite($version,$data);
+    #
 ?>
