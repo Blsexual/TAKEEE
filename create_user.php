1453725eaacd673dcd4934478a786ce3c6397098
Nixeff <theodore.folkesson@gmail.com>
2022-10-24 09:58:19 +0200

    maybe working la in Insert

--- create_user.php
+++ create_user.php
@@ -5,18 +5,51 @@
     if(!empty($_GET["username"])){
         $username = $_GET["username"];
     } else{
-        errorWrite($version,"");
+        errorWrite($version,"No username given");
     }
     if(!empty($_GET["password"])){
         $password = $_GET["password"];
+    } else{
+        errorWrite($version,"No password given");
     }
     if(!empty($_GET["email"])){
         $email = $_GET["email"];
+    } else{
+        errorWrite($version,"No email given");
     }
     if(!empty($_GET["admin"])){
         $admin = $_GET["admin"];
+    } else{
+        errorWrite($version,"Admin privilliges not defined");
     }
     if(!empty($_GET["endUser"])){
         $endUser = $_GET["endUser"];
+    } else{
+        errorWrite($version,"End user privilliges not defined");
     }
+    if(!empty($_GET["desc"])){
+        $description = $_GET["desc"];
+    } else{
+        $description = "Hello!";
+    }
+    if(!empty($_GET["avatar"])){
+        $avatar = $_GET["avatar"];
+    } else{
+        $avatar = "unset";
+    }
+
+    $stmt = $conn->prepare("SELECT * FROM user WHERE name = ?");
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if($result->num_rows > 0){
+        errorWrite($version,"Username already in use");
+    } else{
+        $stmt = $conn->prepare("INSERT INTO user (name,password,email,admin,endUser,description,avatar) values(?,?,?,?,?,?,?)");
+        $stmt->bind_param("sssssss", $username,$password,$email,$admin,$endUser,$description,$avatar);
+        $stmt->execute();
+        $result = $stmt->get_result();
+    }
+
 ?>
