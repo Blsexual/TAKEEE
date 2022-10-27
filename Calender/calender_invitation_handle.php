e2e74c2ec53b14878aa83a3c9b75153d6d4de1ac
BreadWithSoup <erik.post@elev.ga.ntig.se>
2022-10-27 14:58:18 +0200

    lite ändringar i index

--- Calender/calender_invitation_handle.php
+++ Calender/calender_invitation_handle.php
@@ -9,7 +9,8 @@
                 Checks if handle is accept
             ----------------------------------------------------------------------*/
                 if($eventHandle == "accept"){
-                    require("calender_invitation_accept.php");
+                    require_once("calender_invitation_accept.php");
+                    errorWrite($version,"Something went wrong in invitaion accept");
                 }
             #
 
@@ -17,10 +18,19 @@
                 Checks if handle is decline
             ----------------------------------------------------------------------*/
                 if($eventHandle == "decline"){
-                    require("calender_invitation_decline.php");
+                    require_once("calender_invitation_decline.php");
+                    errorWrite($version,"Something went wrong in invitaion decline");
                 }
             #
-            
+
+            /*----------------------------------------------------------------------
+                Checks if handle is cancel
+            ----------------------------------------------------------------------*/
+                if($action == "cancel"){
+                    require_once("calender_invitation_cancel.php");
+                    errorWrite($version,"Something went wrong in invitaion cancel");
+                }
+            #
             errorWrite($version,"Not a valid action, either accept or decline");
         }
         errorWrite($version,"Specify handle");
