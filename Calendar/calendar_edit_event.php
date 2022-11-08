5c7e62062f65b895b2aac870bd224d352ef74079
Bisexual | (Kev) <kevin.johansson@elev.ga.ntig.se>
2022-11-08 09:10:39 +0100

    Error fixes

--- Calendar/calendar_edit_event.php
+++ Calendar/calendar_edit_event.php
@@ -48,24 +48,24 @@
     #
 
     /*----------------------------------------------------------------------
-        Checks each character of the start date and reconsctructs them into a valid string
+        Checks each character of the start date and reconsctructs them into a valid string. Must be formatted as: 0000-00-00 00:00
     ----------------------------------------------------------------------*/
         $newStartDate = NULL;
         if(is_numeric(substr($startDate,0,4))){
             $newStartDate = $newStartDate.(substr($startDate,0,4));
         }
         else{
-            errorWrite($version,"Not a valid date 1");
+            errorWrite($version,"Not a valid Year");
         }
         if(substr($startDate,4,1) == "-"){
             $newStartDate = $newStartDate.(substr($startDate,4,1));
         }
         else{
-            errorWrite($version,"Not a valid date 2");
+            errorWrite($version,"Not a valid connector between Year and Month");
         }
         if(is_numeric(substr($startDate,5,2))){
             if((substr($startDate,5,2) < 1) or ((substr($startDate,5,2) > 12))){
-                errorWrite($version,"Not a valid date 3");
+                errorWrite($version,"Not a valid Month");
             }
             $newStartDate = $newStartDate.(substr($startDate,5,2));
             $dateString = $newStartDate;
@@ -73,54 +73,54 @@
             $lastdate = strtotime(date("Y-m-t", $date ));
         }
         else{
-            errorWrite($version,"Not a valid date 4");
+            errorWrite($version,"Not a valid Month");
         }
         if(substr($startDate,7,1) == "-"){
             $newStartDate = $newStartDate.(substr($startDate,7,1));
         }
         else{
-            errorWrite($version,"Not a valid date 5");
+            errorWrite($version,"Not a valid connector between Month and Day");
         }
         if(is_numeric(substr($startDate,8,2))){
             $day = date("d", $lastdate);
             if((substr($startDate,8,2) < 1) or ((substr($startDate,8,2) > $day))){
-                errorWrite($version,"Not a valid date 6");
+                errorWrite($version,"Not a valid Day");
             }
             $newStartDate = $newStartDate.(substr($startDate,8,2));
         }
         else{
-            errorWrite($version,"Not a valid date 7");
+            errorWrite($version,"Not a valid Day");
         }
         if((substr($startDate,10,1) == "T") or (substr($startDate,10,1) == " ")){
             $newStartDate = $newStartDate.(substr($startDate,10,1));
         }
         else{
-            errorWrite($version,"Not a valid date 8");
+            errorWrite($version,"Not a valid connector between Day and Hour");
         }
         if(is_numeric(substr($startDate,11,2))){
             if((substr($startDate,11,2) < 0) or ((substr($startDate,11,2) > 23))){
-                errorWrite($version,"Not a valid date 9");
+                errorWrite($version,"Not a valid Hour");
             }
             $newStartDate = $newStartDate.(substr($startDate,11,2));
         }
         else{
-            errorWrite($version,"Not a valid date 10");
+            errorWrite($version,"Not a valid Hour");
         }
         if(substr($startDate,13,1) == ":"){
             $newStartDate = $newStartDate.(substr($startDate,13,1));
         }
         else{
-            errorWrite($version,"Not a valid date 11");
+            errorWrite($version,"Not a valid connector between Hour and Minute");
         }
         if(is_numeric(substr($startDate,14,2))){
             if((substr($startDate,14,2) < 0) or ((substr($startDate,14,2) > 59))){
-                errorWrite($version,"Not a valid date 12");
+                errorWrite($version,"Not a valid Minute");
             }
             $newStartDate = $newStartDate.(substr($startDate,14,2));
 
         }
         else{
-            errorWrite($version,"Not a valid date 13");
+            errorWrite($version,"Not a valid Minute");
         }
         $startDate = $newStartDate;
     #
@@ -148,24 +148,24 @@
 
 
     /*----------------------------------------------------------------------
-        Checks each character of the end date and reconsctructs them into a valid string
+        Checks each character of the end date and reconsctructs them into a valid string. Must be formatted as: 0000-00-00 00:00
     ----------------------------------------------------------------------*/
         $newEndDate = NULL;
         if(is_numeric(substr($endDate,0,4))){
             $newEndDate = $newEndDate.(substr($endDate,0,4));
         }
         else{
-            errorWrite($version,"Not a valid date 1");
+            errorWrite($version,"Not a valid Year");
         }
         if(substr($endDate,4,1) == "-"){
             $newEndDate = $newEndDate.(substr($endDate,4,1));
         }
         else{
-            errorWrite($version,"Not a valid date 2");
+            errorWrite($version,"Not a valid connector between Year and Month");
         }
         if(is_numeric(substr($endDate,5,2))){
             if((substr($endDate,5,2) < 1) or ((substr($endDate,5,2) > 12))){
-                errorWrite($version,"Not a valid date 3");
+                errorWrite($version,"Not a valid Month");
             }
             $newEndDate = $newEndDate.(substr($endDate,5,2));
             $dateString = $newEndDate;
@@ -173,53 +173,53 @@
             $lastdate = strtotime(date("Y-m-t", $date ));
         }
         else{
-            errorWrite($version,"Not a valid date 4");
+            errorWrite($version,"Not a valid Month");
         }
         if(substr($endDate,7,1) == "-"){
             $newEndDate = $newEndDate.(substr($endDate,7,1));
         }
         else{
-            errorWrite($version,"Not a valid date 5");
+            errorWrite($version,"Not a valid connector between Month and Day");
         }
         if(is_numeric(substr($endDate,8,2))){
             $day = date("d", $lastdate);
             if((substr($endDate,8,2) < 1) or ((substr($endDate,8,2) > $day))){
-                errorWrite($version,"Not a valid date 6");
+                errorWrite($version,"Not a valid Day");
             }
             $newEndDate = $newEndDate.(substr($endDate,8,2));
         }
         else{
-            errorWrite($version,"Not a valid date 7");
+            errorWrite($version,"Not a valid Day");
         }
         if((substr($endDate,10,1) == "T") or (substr($endDate,10,1) == " ")){
             $newEndDate = $newEndDate.(substr($endDate,10,1));
         }
         else{
-            errorWrite($version,"Not a valid date 8");
+            errorWrite($version,"Not a valid connector between Day and Hour");
         }
         if(is_numeric(substr($endDate,11,2))){
             if((substr($endDate,11,2) < 0) or ((substr($endDate,11,2) > 23))){
-                errorWrite($version,"Not a valid date 9");
+                errorWrite($version,"Not a valid Hour");
             }
             $newEndDate = $newEndDate.(substr($endDate,11,2));
         }
         else{
-            errorWrite($version,"Not a valid date 10");
+            errorWrite($version,"Not a valid Hour");
         }
         if(substr($endDate,13,1) == ":"){
             $newEndDate = $newEndDate.(substr($endDate,13,1));
         }
         else{
-            errorWrite($version,"Not a valid date 11");
+            errorWrite($version,"Not a valid connector between Hour and Minute");
         }
         if(is_numeric(substr($endDate,14,2))){
             if((substr($endDate,14,2) < 0) or ((substr($endDate,14,2) > 59))){
-                errorWrite($version,"Not a valid date 12");
+                errorWrite($version,"Not a valid Minute");
             }
             $newEndDate = $newEndDate.(substr($endDate,14,2));
         }
         else{
-            errorWrite($version,"Not a valid date 13");
+            errorWrite($version,"Not a valid Minute");
         }
         $endDate = $newEndDate;
     #
