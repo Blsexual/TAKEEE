bb19cdecbf258314803e954fd2dc72df68fb8d23
Bisexual | (Kev) <kevin.johansson@elev.ga.ntig.se>
2022-11-22 11:28:02 +0100

    Fixed date messages

--- Calendar/calendar_edit_event.php
+++ Calendar/calendar_edit_event.php
@@ -61,17 +61,17 @@
             $newStartDate = $newStartDate.(substr($startDate,0,4));
         }
         else{
-            errorWrite($version,"Not a valid Year");
+            errorWrite($version,"Year must be 4 numbers (2003)");
         }
         if(substr($startDate,4,1) == "-"){
             $newStartDate = $newStartDate.(substr($startDate,4,1));
         }
         else{
-            errorWrite($version,"Not a valid connector between Year and Month");
+            errorWrite($version,'Must be a "-" between Year and Month');
         }
         if(is_numeric(substr($startDate,5,2))){
             if((substr($startDate,5,2) < 1) or ((substr($startDate,5,2) > 12))){
-                errorWrite($version,"Not a valid Month");
+                errorWrite($version,"Month must be 2 numbers (12)");
             }
             $newStartDate = $newStartDate.(substr($startDate,5,2));
             $dateString = $newStartDate;
@@ -79,54 +79,53 @@
             $lastdate = strtotime(date("Y-m-t", $date ));
         }
         else{
-            errorWrite($version,"Not a valid Month");
+            errorWrite($version,"Month must be 2 numbers (04)");
         }
         if(substr($startDate,7,1) == "-"){
             $newStartDate = $newStartDate.(substr($startDate,7,1));
         }
         else{
-            errorWrite($version,"Not a valid connector between Month and Day");
+            errorWrite($version,'Must be a "-" between Month and Day');
         }
         if(is_numeric(substr($startDate,8,2))){
             $day = date("d", $lastdate);
             if((substr($startDate,8,2) < 1) or ((substr($startDate,8,2) > $day))){
-                errorWrite($version,"Not a valid Day");
+                errorWrite($version,"Day must be 2 numbers (24)");
             }
             $newStartDate = $newStartDate.(substr($startDate,8,2));
         }
         else{
-            errorWrite($version,"Not a valid Day");
+            errorWrite($version,"Day must be 2 numbers (24)");
         }
         if((substr($startDate,10,1) == "T") or (substr($startDate,10,1) == " ")){
             $newStartDate = $newStartDate.(substr($startDate,10,1));
         }
         else{
-            errorWrite($version,"Not a valid connector between Day and Hour");
+            errorWrite($version,'Must be a " " or "T" between Day and Hour');
         }
         if(is_numeric(substr($startDate,11,2))){
             if((substr($startDate,11,2) < 0) or ((substr($startDate,11,2) > 23))){
-                errorWrite($version,"Not a valid Hour");
+                errorWrite($version,"Hour must be 2 numbers (18)");
             }
             $newStartDate = $newStartDate.(substr($startDate,11,2));
         }
         else{
-            errorWrite($version,"Not a valid Hour");
+            errorWrite($version,"hour must be 2 numbers (18)");
         }
         if(substr($startDate,13,1) == ":"){
             $newStartDate = $newStartDate.(substr($startDate,13,1));
         }
         else{
-            errorWrite($version,"Not a valid connector between Hour and Minute");
+            errorWrite($version,'Must be a ":" between Hour and Minute');
         }
         if(is_numeric(substr($startDate,14,2))){
             if((substr($startDate,14,2) < 0) or ((substr($startDate,14,2) > 59))){
-                errorWrite($version,"Not a valid Minute");
+                errorWrite($version,"Minute must be 2 numbers (59)");
             }
             $newStartDate = $newStartDate.(substr($startDate,14,2));
-
         }
         else{
-            errorWrite($version,"Not a valid Minute");
+            errorWrite($version,"Minute must be 2 numbers (59)");
         }
         $startDate = $newStartDate;
     #
@@ -161,17 +160,17 @@
             $newEndDate = $newEndDate.(substr($endDate,0,4));
         }
         else{
-            errorWrite($version,"Not a valid Year");
+            errorWrite($version,"Year must be 4 numbers (2003)");
         }
         if(substr($endDate,4,1) == "-"){
             $newEndDate = $newEndDate.(substr($endDate,4,1));
         }
         else{
-            errorWrite($version,"Not a valid connector between Year and Month");
+            errorWrite($version,'Must be a "-" between Year and Month');
         }
         if(is_numeric(substr($endDate,5,2))){
             if((substr($endDate,5,2) < 1) or ((substr($endDate,5,2) > 12))){
-                errorWrite($version,"Not a valid Month");
+                errorWrite($version,"Month must be 2 numbers (12)");
             }
             $newEndDate = $newEndDate.(substr($endDate,5,2));
             $dateString = $newEndDate;
@@ -179,53 +178,53 @@
             $lastdate = strtotime(date("Y-m-t", $date ));
         }
         else{
-            errorWrite($version,"Not a valid Month");
+            errorWrite($version,"Month must be 2 numbers (04)");
         }
         if(substr($endDate,7,1) == "-"){
             $newEndDate = $newEndDate.(substr($endDate,7,1));
         }
         else{
-            errorWrite($version,"Not a valid connector between Month and Day");
+            errorWrite($version,'Must be a "-" between Month and Day');
         }
         if(is_numeric(substr($endDate,8,2))){
             $day = date("d", $lastdate);
             if((substr($endDate,8,2) < 1) or ((substr($endDate,8,2) > $day))){
-                errorWrite($version,"Not a valid Day");
+                errorWrite($version,"Day must be 2 numbers (24)");
             }
             $newEndDate = $newEndDate.(substr($endDate,8,2));
         }
         else{
-            errorWrite($version,"Not a valid Day");
+            errorWrite($version,"Day must be 2 numbers (24)");
         }
         if((substr($endDate,10,1) == "T") or (substr($endDate,10,1) == " ")){
             $newEndDate = $newEndDate.(substr($endDate,10,1));
         }
         else{
-            errorWrite($version,"Not a valid connector between Day and Hour");
+            errorWrite($version,'Must be a " " or "T" between Day and Hour');
         }
         if(is_numeric(substr($endDate,11,2))){
             if((substr($endDate,11,2) < 0) or ((substr($endDate,11,2) > 23))){
-                errorWrite($version,"Not a valid Hour");
+                errorWrite($version,"Hour must be 2 numbers (18)");
             }
             $newEndDate = $newEndDate.(substr($endDate,11,2));
         }
         else{
-            errorWrite($version,"Not a valid Hour");
+            errorWrite($version,"hour must be 2 numbers (18)");
         }
         if(substr($endDate,13,1) == ":"){
             $newEndDate = $newEndDate.(substr($endDate,13,1));
         }
         else{
-            errorWrite($version,"Not a valid connector between Hour and Minute");
+            errorWrite($version,'Must be a ":" between Hour and Minute');
         }
         if(is_numeric(substr($endDate,14,2))){
             if((substr($endDate,14,2) < 0) or ((substr($endDate,14,2) > 59))){
-                errorWrite($version,"Not a valid Minute");
+                errorWrite($version,"Minute must be 2 numbers (59)");
             }
             $newEndDate = $newEndDate.(substr($endDate,14,2));
         }
         else{
-            errorWrite($version,"Not a valid Minute");
+            errorWrite($version,"Minute must be 2 numbers (59)");
         }
         $endDate = $newEndDate;
     #
